a012b2203e05871713125d491baf113241c03d68
会远 <jiezhouwork@gmail.com>
2021-05-27 14:29:25 +0800

    feat: 修改规则

--- commitlint.config.js
+++ commitlint.config.js
@@ -15,6 +15,13 @@ module.exports = {
         'chore',
         'deps'
       ]
-    ]
+    ],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72]
   }
 };
