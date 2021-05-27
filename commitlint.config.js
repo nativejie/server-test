0f4c51b6e7da11e30b0a6ab0071a8e5608b8e23a
会远 <jiezhouwork@gmail.com>
2021-05-27 14:25:40 +0800

    feat: 规则配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-emun': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'deps'
+      ]
+    ]
+  }
 };
